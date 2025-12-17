e97558a600ddef907ee24a0bf0cd857b67cd8ea1
Craig <158543768+craigalodon@users.noreply.github.com>
2025-12-17 10:51:17 -0600

    Finished part one of day seven.

---
+++ cmd/dayseven/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	if len(os.Args) < 2 {
+		return fmt.Errorf("usage: go run . <path/to/input/file>")
+	}
+
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		return fmt.Errorf("error opening file: %w", err)
+	}
+	defer func() {
+		if err := file.Close(); err != nil {
+			fmt.Fprintf(os.Stderr, "Error closing file: %v\n", err)
+		}
+	}()
+
+	scanner := bufio.NewScanner(file)
+
+	beams := make(map[int]bool)
+	splits := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		for i, r := range line {
+			if r == 'S' {
+				beams[i] = true
+				continue
+			}
+			if r == '^' && beams[i] {
+				splits++
+				beams[i] = false
+				beams[i-1] = true
+				beams[i+1] = true
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return fmt.Errorf("error reading file: %w", err)
+	}
+
+	fmt.Printf("Splits: %v\n", splits)
+
+	return nil
+}
