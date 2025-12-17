1026c8d87b31735d3217d39963f2b5b8de5bd33f
Craig <158543768+craigalodon@users.noreply.github.com>
2025-12-17 00:34:09 -0600

    Solved part two.

--- cmd/daysix/main.go
+++ cmd/daysix/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
-	"adventofcode2025/internal/delimited"
 	"bufio"
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
+
+type Col struct {
+	Offset int
+	Op     rune
+	Grid   []string
+}
 
 func main() {
 	if err := run(); err != nil {
@@ -20,76 +26,174 @@ func run() error {
 		return fmt.Errorf("usage: go run . <path/to/input/file>")
 	}
 
-	file, err := os.Open(os.Args[1])
+	rows, err := readFile(os.Args[1])
 	if err != nil {
-		return fmt.Errorf("error opening file: %w", err)
+		return err
 	}
-	defer func() {
-		if err := file.Close(); err != nil {
-			fmt.Fprintf(os.Stderr, "Error closing file: %v\n", err)
-		}
-	}()
 
-	scanner := bufio.NewScanner(file)
+	cols := initCols(rows[len(rows)-1])
+	populateGrids(rows[:len(rows)-1], cols)
+	leftJustifyGrid(cols[len(cols)-1].Grid)
 
-	vals := make([][]int, 0)
+	part1 := 0
+	part2 := 0
 
-	for scanner.Scan() {
-		line := scanner.Text()
-		row := delimited.ParseSpaceDelimited(line)
-		ints, success := tryConvertRowToInts(row)
-		if success {
-			vals = append(vals, ints)
-		} else {
-			sum, err := aggregate(row, vals)
+	for _, col := range cols {
+		{
+			roworder, err := getRowOrderValues(col)
 			if err != nil {
-				return err
+				return fmt.Errorf("error during row-order traversal: %w", err)
 			}
-			fmt.Printf("Sum: %v\n", sum)
+			res, err := agg(col, roworder)
+			if err != nil {
+				return fmt.Errorf("unable to aggregate row-order values: %w", err)
+			}
+			part1 += res
+		}
+
+		{
+			colorder, err := getColOrderValues(col)
+			if err != nil {
+				return fmt.Errorf("error during col-order traversal: %w", err)
+			}
+			res, err := agg(col, colorder)
+			if err != nil {
+				return fmt.Errorf("unable to aggregate col-order values: %w", err)
+			}
+			part2 += res
 		}
 	}
+
+	fmt.Printf("Row-Order Traversal: %v\n", part1)
+	fmt.Printf("Column-Order Traversal: %v\n", part2)
 
 	return nil
 }
 
-func tryConvertRowToInts(row []string) ([]int, bool) {
-	vals := make([]int, 0)
-	for _, s := range row {
-		v, err := strconv.Atoi(s)
-		if err != nil {
-			return nil, false
+func agg(c Col, vals []int) (int, error) {
+	if c.Op != '+' && c.Op != '*' {
+		return 0, fmt.Errorf("invalid op value: %v", c.Op)
+	}
+
+	acc := 0
+	if c.Op == '*' {
+		acc = 1
+	}
+
+	for _, v := range vals {
+		if c.Op == '+' {
+			acc += v
 		} else {
-			vals = append(vals, v)
+			acc *= v
 		}
 	}
 
-	return vals, true
+	return acc, nil
 }
 
-func aggregate(ops []string, vals [][]int) (int, error) {
-	sum := 0
-
-	for i, r := range ops {
-		if r == "+" {
-			acc := 0
-			for _, row := range vals {
-				acc += row[i]
-			}
-			sum += acc
-			continue
+func getColOrderValues(c Col) ([]int, error) {
+	vals := make([]int, 0)
+	for i := range len(c.Grid[0]) {
+		builder := strings.Builder{}
+		for j := range len(c.Grid) {
+			builder.WriteByte(c.Grid[j][i])
 		}
-
-		if r == "*" {
-			acc := 1
-			for _, row := range vals {
-				acc *= row[i]
-			}
-			sum += acc
-			continue
+		s := builder.String()
+		val, err := trimThenAtoi(s)
+		if err != nil {
+			return nil, fmt.Errorf("unable to parse column: %w", err)
 		}
+		vals = append(vals, val)
+	}
+	return vals, nil
+}
 
-		return 0, fmt.Errorf("unexpected operator: %s", r)
+func getRowOrderValues(c Col) ([]int, error) {
+	vals := make([]int, 0)
+	for _, s := range c.Grid {
+		val, err := trimThenAtoi(s)
+		if err != nil {
+			return nil, fmt.Errorf("unable to parse row: %w", err)
+		}
+		vals = append(vals, val)
 	}
 
-	return sum, nil
+	return vals, nil
+}
+
+func trimThenAtoi(s string) (int, error) {
+	prev := 0
+	for _, r := range s {
+		if r != ' ' {
+			curr, err := strconv.Atoi(string(r))
+			if err != nil {
+				return 0, fmt.Errorf("unable to convert to int: %w", err)
+			}
+			prev = prev*10 + curr
+		}
+	}
+	return prev, nil
+}
+
+func leftJustifyGrid(grid []string) {
+	max := len(grid[0])
+	for i := 1; i < len(grid); i++ {
+		curr := len(grid[i])
+		if curr > max {
+			max = curr
+		}
+	}
+
+	for i := range len(grid) {
+		curr := len(grid[i])
+		if curr < max {
+			padding := max - curr
+			grid[i] = grid[i] + strings.Repeat(" ", padding)
+		}
+	}
+}
+
+func populateGrids(rows []string, cols []Col) {
+	for _, row := range rows {
+		for i, col := range cols {
+			next := len(row)
+			if i != len(cols)-1 {
+				next = cols[i+1].Offset - 1
+			}
+			cols[i].Grid = append(cols[i].Grid, row[col.Offset:next])
+		}
+	}
+}
+
+func initCols(row string) []Col {
+	cols := make([]Col, 0)
+
+	for i, r := range row {
+		if r == '*' || r == '+' {
+			cols = append(cols, Col{Offset: i, Op: r})
+		}
+	}
+
+	return cols
+}
+
+func readFile(filepath string) ([]string, error) {
+	file, err := os.Open(filepath)
+	if err != nil {
+		return nil, fmt.Errorf("error opening file: %w", err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	rows := make([]string, 0)
+
+	for scanner.Scan() {
+		row := scanner.Text()
+		rows = append(rows, row)
+	}
+
+	if err := file.Close(); err != nil {
+		return nil, fmt.Errorf("error closing file: %w", err)
+	}
+
+	return rows, nil
 }
