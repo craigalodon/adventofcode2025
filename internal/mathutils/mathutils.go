ebb4acd89c6a9d9fdb5515ce09a79e0a67a964f9
Craig <158543768+craigalodon@users.noreply.github.com>
2026-01-18 21:45:59 -0600

    Added helper functions to find the pivots, free variables, and parametrize a system of linear equations, with their associated tests.

--- internal/mathutils/mathutils.go
+++ internal/mathutils/mathutils.go
@@ -3,6 +3,7 @@ package mathutils
 import (
 	"fmt"
 	"math"
+	"slices"
 )
 
 func Mod(a, b int) int {
@@ -60,15 +61,12 @@ func CountDigits(n int) int {
 }
 
 func matrixReduce(matrix [][]float64) error {
-	m := len(matrix)
-	n := len(matrix[0])
-
 	h := 0
 	k := 0
 
-	for h < m && k < n {
+	for h < len(matrix) && k < len(matrix[0]) {
 		iMax := h
-		for i := h + 1; i < m; i++ {
+		for i := h + 1; i < len(matrix); i++ {
 			if math.Abs(matrix[i][k]) > math.Abs(matrix[iMax][k]) {
 				iMax = i
 			}
@@ -79,15 +77,15 @@ func matrixReduce(matrix [][]float64) error {
 		} else {
 			matrix[h], matrix[iMax] = matrix[iMax], matrix[h]
 
-			for i := h + 1; i < m; i++ {
+			for i := h + 1; i < len(matrix); i++ {
 				f := matrix[i][k] / matrix[h][k]
 				matrix[i][k] = 0
-				for j := k + 1; j < n; j++ {
+				for j := k + 1; j < len(matrix[0]); j++ {
 					matrix[i][j] = matrix[i][j] - matrix[h][j]*f
 				}
 			}
 
-			for i := n - 1; i >= k; i-- {
+			for i := len(matrix[0]) - 1; i >= k; i-- {
 				matrix[h][i] = matrix[h][i] / matrix[h][k]
 			}
 
@@ -100,7 +98,7 @@ func matrixReduce(matrix [][]float64) error {
 
 	for h > 0 {
 		k = 0
-		for range n {
+		for range len(matrix[0]) {
 			if matrix[h][k] == 1 {
 				break
 			}
@@ -112,14 +110,14 @@ func matrixReduce(matrix [][]float64) error {
 			continue
 		}
 
-		if k == n-1 {
+		if k == len(matrix[0])-1 {
 			return fmt.Errorf("matrix is inconsistent")
 		}
 
 		for i := 0; i < h; i++ {
 			if !isZero(matrix[i][k]) {
 				f := matrix[i][k]
-				for j := k; j < n; j++ {
+				for j := k; j < len(matrix[0]); j++ {
 					matrix[i][j] = matrix[i][j] - matrix[h][j]*f
 				}
 			}
@@ -129,6 +127,71 @@ func matrixReduce(matrix [][]float64) error {
 	}
 
 	return nil
+}
+
+func findPivots(matrix [][]float64) map[int]int {
+	pivots := make(map[int]int)
+
+	h := 0
+	k := 0
+
+	for h < len(matrix) && k < len(matrix[0])-1 {
+		if isZero(matrix[h][k]) {
+			k++
+		} else {
+			pivots[k] = h
+			h++
+			k++
+		}
+	}
+	return pivots
+}
+
+func findFreeVariables(matrix [][]float64, pivots map[int]int) map[int]int {
+	freeVariables := make(map[int]int)
+	arr := make([]int, 0)
+	for k, h := range pivots {
+		for j := k + 1; j < len(matrix[0])-1; j++ {
+			if !isZero(matrix[h][j]) {
+				if _, ok := freeVariables[j]; !ok {
+					freeVariables[j] = 0
+					arr = append(arr, j)
+				}
+			}
+		}
+	}
+
+	slices.Sort(arr)
+	for i, j := range arr {
+		freeVariables[j] = i
+	}
+
+	return freeVariables
+}
+
+func parametrize(matrix [][]float64) [][]float64 {
+	pivots := findPivots(matrix)
+	freeVariables := findFreeVariables(matrix, pivots)
+
+	params := make([][]float64, len(matrix[0])-1)
+	for i := 0; i < len(params); i++ {
+		params[i] = make([]float64, len(freeVariables)+1)
+	}
+
+	for k, h := range pivots {
+		params[k][0] = matrix[h][len(matrix[0])-1]
+		for i, j := range freeVariables {
+			if i > k {
+				params[k][j+1] = matrix[h][i] * -1
+			}
+		}
+	}
+
+	for i, j := range freeVariables {
+		params[i][j+1] = 1
+	}
+
+	return params
 }
 
 func isZero(f float64) bool {
--- internal/mathutils/mathutils_test.go
+++ internal/mathutils/mathutils_test.go
@@ -78,14 +78,208 @@ func TestMatrixReduce(t *testing.T) {
 
 			if tt.err != nil {
 				if err == nil || err.Error() != tt.err.Error() {
-					t.Errorf("MatrixReduce() error = %v, expected %v", err, tt.err)
+					t.Errorf("matrixReduce() error = %v, expected %v", err, tt.err)
 				}
 			} else if err != nil {
-				t.Errorf("MatrixReduce() unexpected error = %v", err)
+				t.Errorf("matrixReduce() unexpected error = %v", err)
 			} else {
 				if !reflect.DeepEqual(tt.matrix, tt.expected) {
-					t.Errorf("MatrixReduce() = %v, want %v", tt.matrix, tt.expected)
+					t.Errorf("matrixReduce() = %v, want %v", tt.matrix, tt.expected)
 				}
+			}
+		})
+	}
+}
+
+func TestFindPivots(t *testing.T) {
+	tests := []struct {
+		name     string
+		matrix   [][]float64
+		expected map[int]int
+	}{
+		{
+			name: "Matrix 1",
+			matrix: [][]float64{
+				{1, 0, 0, 1, 0, -1, 2},
+				{0, 1, 0, 0, 0, 1, 5},
+				{0, 0, 1, 1, 0, -1, 1},
+				{0, 0, 0, 0, 1, 1, 3}},
+			expected: map[int]int{
+				0: 0,
+				1: 1,
+				2: 2,
+				4: 3},
+		},
+		{
+			name: "Matrix 2",
+			matrix: [][]float64{
+				{1, 0, 1, 0, 0, 2},
+				{0, 1, -1, 0, 0, 5},
+				{0, 0, 0, 1, 0, 5},
+				{0, 0, 0, 0, 1, 0},
+				{0, 0, 0, 0, 0, 0}},
+			expected: map[int]int{
+				0: 0,
+				1: 1,
+				3: 2,
+				4: 3},
+		},
+		{
+			name: "Matrix 3",
+			matrix: [][]float64{
+				{1, 0, 0, 0, 0, 0, 5},
+				{0, 1, 0, 0, 0, 0, 0},
+				{0, 0, 1, 0, 0, 0, 5},
+				{0, 0, 0, 1, 0, 0, 1},
+				{0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0}},
+			expected: map[int]int{
+				0: 0,
+				1: 1,
+				2: 2,
+				3: 3},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			pivots := findPivots(tt.matrix)
+			if !reflect.DeepEqual(pivots, tt.expected) {
+				t.Errorf("findPivots() = %v, want %v", pivots, tt.expected)
+			}
+		})
+	}
+}
+
+func TestFindFreeVariables(t *testing.T) {
+	tests := []struct {
+		name     string
+		matrix   [][]float64
+		pivots   map[int]int
+		expected map[int]int
+	}{
+		{
+			name: "Matrix 1",
+			matrix: [][]float64{
+				{1, 0, 0, 1, 0, -1, 2},
+				{0, 1, 0, 0, 0, 1, 5},
+				{0, 0, 1, 1, 0, -1, 1},
+				{0, 0, 0, 0, 1, 1, 3}},
+			pivots: map[int]int{
+				0: 0,
+				1: 1,
+				2: 2,
+				4: 3},
+			expected: map[int]int{
+				3: 0,
+				5: 1},
+		},
+		{
+			name: "Matrix 2",
+			matrix: [][]float64{
+				{1, 0, 1, 0, 0, 2},
+				{0, 1, -1, 0, 0, 5},
+				{0, 0, 0, 1, 0, 5},
+				{0, 0, 0, 0, 1, 0},
+				{0, 0, 0, 0, 0, 0}},
+			pivots: map[int]int{
+				0: 0,
+				1: 1,
+				3: 2,
+				4: 3},
+			expected: map[int]int{2: 0},
+		},
+		{
+			name: "Matrix 3",
+			matrix: [][]float64{
+				{1, 0, 0, 0, 0, 0, 5},
+				{0, 1, 0, 0, 0, 0, 0},
+				{0, 0, 1, 0, 0, 0, 5},
+				{0, 0, 0, 1, 0, 0, 1},
+				{0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0}},
+			pivots: map[int]int{
+				0: 0,
+				1: 1,
+				2: 2,
+				3: 3},
+			expected: map[int]int{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			freeVariables := findFreeVariables(tt.matrix, tt.pivots)
+			if !reflect.DeepEqual(freeVariables, tt.expected) {
+				t.Errorf("findFreeVariables() = %v, want %v", freeVariables, tt.expected)
+			}
+		})
+	}
+}
+
+func TestParametrize(t *testing.T) {
+	tests := []struct {
+		name     string
+		matrix   [][]float64
+		expected [][]float64
+	}{
+		{
+			name: "Matrix 1",
+			matrix: [][]float64{
+				{1, 0, 0, 1, 0, -1, 2},
+				{0, 1, 0, 0, 0, 1, 5},
+				{0, 0, 1, 1, 0, -1, 1},
+				{0, 0, 0, 0, 1, 1, 3}},
+			expected: [][]float64{
+				{2, -1, 1},
+				{5, 0, -1},
+				{1, -1, 1},
+				{0, 1, 0},
+				{3, 0, -1},
+				{0, 0, 1},
+			},
+		},
+		{
+			name: "Matrix 2",
+			matrix: [][]float64{
+				{1, 0, 1, 0, 0, 2},
+				{0, 1, -1, 0, 0, 5},
+				{0, 0, 0, 1, 0, 5},
+				{0, 0, 0, 0, 1, 0},
+				{0, 0, 0, 0, 0, 0}},
+			expected: [][]float64{
+				{2, -1},
+				{5, 1},
+				{0, 1},
+				{5, 0},
+				{0, 0},
+			},
+		},
+		{
+			name: "Matrix 3",
+			matrix: [][]float64{
+				{1, 0, 0, 0, 0, 0, 5},
+				{0, 1, 0, 0, 0, 0, 0},
+				{0, 0, 1, 0, 0, 0, 5},
+				{0, 0, 0, 1, 0, 0, 1},
+				{0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0}},
+			expected: [][]float64{
+				{5},
+				{0},
+				{5},
+				{1},
+				{0},
+				{0},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			params := parametrize(tt.matrix)
+			if !reflect.DeepEqual(params, tt.expected) {
+				t.Errorf("parametrize() = %v, want %v", params, tt.expected)
 			}
 		})
 	}
