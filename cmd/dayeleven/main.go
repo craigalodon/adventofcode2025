ebe850d47b6cc222f935c28dad14918b9df7fe69
Craig <158543768+craigalodon@users.noreply.github.com>
2026-01-20 21:55:59 -0600

    Polished deserialization logic and basic data structure. Added a test to confirm cyclic graph is correctly built.

--- cmd/dayeleven/main.go
+++ cmd/dayeleven/main.go
@@ -1,28 +1,88 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	println("Hello, Day Eleven!")
+	if err := run(); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	if len(os.Args) < 2 {
+		return fmt.Errorf("usage: go run . <path/to/input/file>")
+	}
+
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		return fmt.Errorf("error opening file: %w", err)
+	}
+	defer func() {
+		if err := file.Close(); err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "Error closing file: %v\n", err)
+		}
+	}()
+
+	scanner := bufio.NewScanner(file)
+	parser := NewParser()
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		_, err := parser.Deserialize(line)
+		if err != nil {
+			return fmt.Errorf("error deserializing: %v", err)
+		}
+	}
+
+	root, err := parser.GetRoot()
+	if err != nil {
+		return fmt.Errorf("error getting root: %w", err)
+	}
+
+	seen := make(map[string]bool)
+
+	var recursiveAdd func(n *Node)
+	recursiveAdd = func(n *Node) {
+		if seen[n.Name] {
+			return
+		}
+		seen[n.Name] = true
+		for _, child := range n.children {
+			recursiveAdd(child)
+		}
+	}
+
+	recursiveAdd(root)
+
+	println("Seen: ", len(seen))
+
+	return nil
 }
 
 type Node struct {
 	Name     string
-	children []*Node
+	children map[string]*Node
 }
 
 func NewNode(name string) *Node {
 	return &Node{
 		Name:     name,
-		children: []*Node{},
+		children: make(map[string]*Node),
 	}
 }
 
-func (n *Node) AddChild(child *Node) {
-	n.children = append(n.children, child)
+func (n *Node) AddChild(child *Node) error {
+	if _, ok := n.children[child.Name]; ok {
+		return fmt.Errorf("child %s already exists", child.Name)
+	}
+	n.children[child.Name] = child
+	return nil
 }
 
 func (n *Node) ForEachChild(fn func(n *Node)) {
@@ -32,89 +92,169 @@ func (n *Node) ForEachChild(fn func(n *Node)) {
 }
 
 type Parser struct {
-	state    ParserState
-	builder  strings.Builder
-	Node     *Node
-	children map[string]*Node
-	seen     map[string]*Node
+	state   ParserState
+	builder strings.Builder
+	curr    *Node
+	seen    map[string]*Node
 }
 
-func NewParser(seen map[string]*Node) *Parser {
+func NewParser() *Parser {
 	return &Parser{
-		state:    readingName,
-		builder:  strings.Builder{},
-		children: make(map[string]*Node),
-		seen:     seen,
+		state:   readingName,
+		builder: strings.Builder{},
+		seen:    make(map[string]*Node),
 	}
 }
 
-func (p *Parser) WriteRune(r rune) {
+func (p *Parser) GetNode(name string) (*Node, error) {
+	if node, ok := p.seen[name]; ok {
+		return node, nil
+	}
+
+	return nil, fmt.Errorf("curr not found: %s", name)
+}
+
+func (p *Parser) GetRoot() (*Node, error) {
+	root, err := p.GetNode("you")
+	if err != nil {
+		return nil, fmt.Errorf("root not found")
+	}
+
+	return root, nil
+}
+
+func (p *Parser) Deserialize(s string) (*Node, error) {
+	p.clearCurrent()
+	p.state = readingName
+
+	for _, r := range s {
+		err := p.parse(r)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if p.state == readingChild {
+		err := p.createChild()
+		if err != nil {
+			return nil, err
+		}
+
+		curr, err := p.getCurrent()
+		if err != nil {
+			return nil, err
+		}
+
+		return curr, nil
+	}
+
+	return nil, fmt.Errorf("invalid state")
+}
+
+func (p *Parser) parse(r rune) error {
+	switch r {
+	case ':':
+		err := p.setCurrent()
+		if err != nil {
+			return err
+		}
+	case ' ':
+		switch p.state {
+		case readingChild:
+			err := p.createChild()
+			if err != nil {
+				return err
+			}
+		case seekingChild:
+		default:
+			return fmt.Errorf("invalid character '%c'", r)
+		}
+	default:
+		p.writeRune(r)
+		if p.state == seekingChild {
+			p.state = readingChild
+		}
+	}
+
+	return nil
+}
+
+func (p *Parser) writeRune(r rune) {
 	p.builder.WriteRune(r)
 }
 
-func (p *Parser) GetString() string {
+func (p *Parser) getString() string {
 	return p.builder.String()
 }
 
-func (p *Parser) ResetString() {
+func (p *Parser) resetString() {
 	p.builder.Reset()
 }
 
-func (p *Parser) CreateChild() error {
+func (p *Parser) createChild() error {
 	if p.state != readingChild {
 		return fmt.Errorf("invalid state")
 	}
 
-	name := p.GetString()
-
-	if p.Node.Name == name {
-		return fmt.Errorf("node cannot be own parent")
-	}
-	if _, ok := p.children[name]; ok {
-		return fmt.Errorf("child %s already exists", name)
+	parent, err := p.getCurrent()
+	if err != nil {
+		return err
 	}
 
-	if child, ok := p.seen[name]; ok {
-		p.children[name] = child
+	name := p.getString()
+
+	if parent.Name == name {
+		return fmt.Errorf("curr cannot be own parent")
+	}
+
+	var child *Node
+	if _, ok := p.seen[name]; !ok {
+		child = NewNode(name)
+		p.seen[name] = child
 	} else {
-		p.children[name] = NewNode(name)
-		p.seen[name] = p.children[name]
+		child = p.seen[name]
 	}
 
-	p.Node.AddChild(p.children[name])
+	err = parent.AddChild(child)
+	if err != nil {
+		return err
+	}
 
 	p.state = seekingChild
-	p.ResetString()
+	p.resetString()
 
 	return nil
 }
 
-func (p *Parser) SetName() error {
-	if p.state != readingName {
+func (p *Parser) setCurrent() error {
+	if p.state != readingName || p.curr != nil {
 		return fmt.Errorf("invalid state")
 	}
 
-	name := p.GetString()
+	name := p.getString()
 
 	if ptr, ok := p.seen[name]; ok {
-		p.Node = ptr
+		p.curr = ptr
 	} else {
-		p.Node = NewNode(name)
-		p.seen[name] = p.Node
+		p.curr = NewNode(name)
+		p.seen[name] = p.curr
 	}
 
 	p.state = seekingChild
-	p.ResetString()
+	p.resetString()
 
 	return nil
 }
 
-func (p *Parser) GetNode() *Node {
-	for _, child := range p.children {
-		p.Node.AddChild(child)
+func (p *Parser) getCurrent() (*Node, error) {
+	if p.curr == nil {
+		return nil, fmt.Errorf("current not found")
 	}
+	return p.curr, nil
+}
 
-	return p.Node
+func (p *Parser) clearCurrent() {
+	p.curr = nil
 }
 
 type ParserState int
@@ -124,49 +264,3 @@ const (
 	seekingChild
 	readingChild
 )
-
-func Deserialize(s string, seen map[string]*Node) (*Node, error) {
-	p := NewParser(seen)
-
-	for _, r := range s {
-		err := p.Parse(r)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	err := p.CreateChild()
-	if err != nil {
-		return nil, err
-	}
-
-	return p.Node, nil
-}
-
-func (p *Parser) Parse(r rune) error {
-	switch r {
-	case ':':
-		err := p.SetName()
-		if err != nil {
-			return err
-		}
-	case ' ':
-		switch p.state {
-		case readingChild:
-			err := p.CreateChild()
-			if err != nil {
-				return err
-			}
-		case seekingChild:
-		default:
-			return fmt.Errorf("invalid character '%c'", r)
-		}
-	default:
-		p.WriteRune(r)
-		if p.state == seekingChild {
-			p.state = readingChild
-		}
-	}
-
-	return nil
-}
--- cmd/dayeleven/main_test.go
+++ cmd/dayeleven/main_test.go
@@ -44,13 +44,19 @@ func TestDeserialize(t *testing.T) {
 		{
 			name:  "Parent Of Self",
 			s:     "aaa: aaa",
-			error: errors.New("node cannot be own parent"),
+			error: errors.New("curr cannot be own parent"),
+		},
+		{
+			name:  "Duplicate Children",
+			s:     "aaa: bbb bbb",
+			error: errors.New("child bbb already exists"),
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			result, err := Deserialize(tt.s, map[string]*Node{})
+			p := NewParser()
+			result, err := p.Deserialize(tt.s)
 			if tt.error != nil {
 				if err.Error() != tt.error.Error() {
 					t.Errorf("Deserialize() error = %v, wantErr %v", err, tt.error)
@@ -69,5 +75,55 @@ func TestDeserialize(t *testing.T) {
 				}
 			}
 		})
+	}
+}
+
+func TestDeserializeCycle(t *testing.T) {
+	data := []string{
+		"you: aaa",
+		"aaa: bbb",
+		"bbb: ccc",
+		"ccc: you",
+	}
+
+	p := NewParser()
+
+	for _, d := range data {
+		_, err := p.Deserialize(d)
+		if err != nil {
+			t.Errorf("Deserialize() error = %v, wantErr %v", err, nil)
+		}
+	}
+
+	root, err := p.GetRoot()
+	if err != nil {
+		t.Errorf("GetRoot() error = %v, wantErr %v", err, nil)
+	}
+
+	var found bool
+	var recursiveSearch func(*Node, int)
+	recursiveSearch = func(n *Node, depth int) {
+		if found {
+			return
+		}
+
+		if n == root && depth > 0 {
+			found = true
+			return
+		}
+
+		if depth > 10 {
+			return
+		}
+
+		n.ForEachChild(func(child *Node) {
+			recursiveSearch(child, depth+1)
+		})
+	}
+
+	recursiveSearch(root, 0)
+
+	if found != true {
+		t.Errorf("Deserialize() = %v, want %v", found, true)
 	}
 }
